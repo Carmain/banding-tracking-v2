97a1152c42196027635d498eb251f742a7d50ea3
Billard Martin <martin.l.billard@gmail.com>
2016-04-01 14:42:47 +0200

    Update the template configuration

--- banding_tracking/settings.py
+++ banding_tracking/settings.py
@@ -60,6 +60,23 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'banding_tracking.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.i18n'
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'banding_tracking.wsgi.application'
 
 
