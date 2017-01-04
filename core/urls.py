632744260bb79eed977b865cbbc3561b699805c9
Billard Martin <martin.l.billard@gmail.com>
2017-01-04 15:16:17 +0100

    Add and link all the basic pages

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^administration$', views.administration, name='administration'),
+    url(r'^new-observation$', views.observation_form, name='observation_form'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'core/home.html')
+
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def administration(request):
+    return render(request, 'core/administration.html')
+
+
+def observation_form(request):
+    return render(request, 'core/observation_form.html')
