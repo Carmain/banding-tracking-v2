d4c6e5ea80e681ba7596470cf93842cbe111d20d
Billard Martin <martin.l.billard@gmail.com>
2017-01-11 16:28:02 +0100

    Pass args from a form to another

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.http import HttpResponse, HttpResponseRedirect
 
 
@@ -21,7 +22,17 @@ def map(request):
 def observations(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        return render(request, 'core/observations.html')
+        location = {
+            'date': request.POST.get('date'),
+            'town': request.POST.get('town'),
+            'department': request.POST.get('department'),
+            'country': request.POST.get('country'),
+            'location': request.POST.get('location'),
+            'coordinate_x': request.POST.get('coordinate_x'),
+            'coordinate_y': request.POST.get('coordinate_y')
+        }
+
+        return render(request, 'core/observations.html', location)
     else:
         # redirect to a new URL:
         return HttpResponseRedirect('map')
